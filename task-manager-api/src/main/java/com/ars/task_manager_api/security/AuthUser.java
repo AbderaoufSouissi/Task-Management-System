c15f984317e5c54236ea90cbc417de881b532e17
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-06-25 18:33:46 +0100

    Backend: implement initial Jwt security config

---
+++ task-manager-api/src/main/java/com/ars/task_manager_api/security/AuthUser.java
@@ -0,0 +1,36 @@
+package com.ars.task_manager_api.security;
+
+import com.ars.task_manager_api.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthUser implements UserDetails {
+    private User user;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
---
+++ task-manager-api/src/main/java/com/ars/task_manager_api/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.ars.task_manager_api.security;
+
+import com.ars.task_manager_api.entity.User;
+import com.ars.task_manager_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
